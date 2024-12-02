facc35c26eb12376a5b61a1f9050be90bd3d02bb
mahmoudhafez103 <hafz7062@gmail.com>
2024-12-02 15:18:01 +0200

    PayMob integration

--- config/services.php
+++ config/services.php
@@ -30,5 +30,10 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
+    'BAYMOB_BASE_URL'=>env('BAYMOB_BASE_URL'),
+
+
+
+    'BAYMOB_API_KEY'=>env('BAYMOB_API_KEY'),
 
 ];
